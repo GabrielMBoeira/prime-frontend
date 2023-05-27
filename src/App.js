2446cbaf4a78ed3a6b49079c79cf0ae2a4a0bc87
Gabriel Machado Boeira <gabriel.boeira@softplan.com.br>
2023-05-27 00:38:07 -0300

    Importação de react-router-dom e gerenciando links/rotas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import './App.css';
-import Home from './pages/Home';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import MyRouter from './router/index'; 
 
 function App() {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+   return (
+      <div>
+         <Link to="/">Home</Link>
+         <Link to="/about-us">About Us</Link>
+         <Link to="/contact-us">Contact</Link>
+         <MyRouter />
+      </div>
+   );
 }
 
 export default App;
