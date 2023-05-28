e02a63bfce00d65dd91573fdf799b65c5b630cc0
Gabriel Machado Boeira <gabriel.boeira@softplan.com.br>
2023-05-28 01:24:16 -0300

    Implementando post request

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,10 +1,14 @@
+import Style from './Loading.css';
 
 function Loading() {
 
     return (
 
-        <div className="spinner-border text-primary" role="status">
-            <span className="visually-hidden">Loading...</span>
+        <div className="loading">
+            <div className="spinner-border text-primary d-flex justify-content-center align-items-center" role="status">
+                <span className="visually-hidden">Loading... </span>
+            </div>
+            <span>Loading... Please wait.</span>
         </div>
     )
 }
--- src/pages/RevenueCreate.js
+++ src/pages/RevenueCreate.js
@@ -1,6 +1,61 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Loading from '../components/Loading';
 
 function RevenueCreate() {
+
+
+    const [loading, setLoading] = useState(false)
+    const [inputErrorList, setInputErrorList] = useState({})
+    const [revenue, setRevenue] = useState({
+        description: '',
+        revenue: '',
+        date: ''
+    })
+
+    const handleInput = (e) => {
+        e.persist();
+        setRevenue({ ...revenue, [e.target.name]: e.target.value })
+    }
+
+    const saveRevenue = (e) => {
+
+        e.preventDefault();
+        setLoading(true);
+
+        const data = {
+            description: revenue.description,
+            revenue: revenue.revenue,
+            date: revenue.date,
+        }
+
+        axios.post(`http://localhost:8000/api/revenue`)
+            .then(res => {
+                alert(res.data.message)
+            })
+            .catch(function (error) {
+
+                if (error.response) {
+
+                    if (error.response.status === 422) {
+                        setInputErrorList(error.response.data.setInputErrorList)
+                        setLoading(true);
+                    }
+
+                    if (error.response.status === 500) {
+                        alert(error.response.data)
+                        setLoading(true);
+                    }
+                }
+            })
+    }
+
+    if (loading) {
+        return (
+            <Loading />
+        )
+    }   
 
     return (
         <div className="container mt-5">
@@ -13,19 +68,22 @@ function RevenueCreate() {
                             </h4>
                         </div>
                         <div className="card-body">
-                            <form>
+                            <form onSubmit={saveRevenue}>
                                 <div className="mb-3">
                                     <label className="form-label">Description</label>
-                                    <input type="text" className="form-control" id="description" />
+                                    <input type="text" className="form-control" name="description" value={revenue.description} onChange={handleInput} />
+                                    <small className='text-danger'>{inputErrorList.description}</small>
                                 </div>
                                 <div className="row">
                                     <div className="mb-3 col-md-4">
                                         <label className="form-label">Revenue</label>
-                                        <input type="text" className="form-control" id="revenue" />
+                                        <input type="text" className="form-control" name="revenue" value={revenue.revenue} onChange={handleInput} />
+                                        <small className='text-danger'>{inputErrorList.revenue}</small>
                                     </div>
                                     <div className="mb-3 col-md-4">
                                         <label className="form-label" >Date</label>
-                                        <input type="date" className="form-control" id="date" />
+                                        <input type="date" className="form-control" name="date" value={revenue.date} onChange={handleInput} />
+                                        <small className='text-danger'>{inputErrorList.date}</small>
                                     </div>
                                 </div>
                                 <div className="d-flex justify-content-center">
